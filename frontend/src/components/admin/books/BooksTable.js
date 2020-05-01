b549abea9465cfac1b0dfb831e0c938c1c082475
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-05-02 00:24:49 +0200

    basic admin panel

---
+++ frontend/src/components/admin/books/BooksTable.js
@@ -0,0 +1,82 @@
+import React, { Component, useM } from "react";
+import { connect } from "react-redux";
+import ReactTable from "react-table-v6";
+import "react-table-v6/react-table.css";
+import { Button } from "reactstrap";
+
+import { updateAdminProps } from "../../../actions/adminAction";
+
+class BooksTable extends Component {
+  editBookModal = (book) => {
+    this.props.updateAdminProps([
+      { prop: "currentBook", value: book },
+      {
+        prop: "isBookModal",
+        value: !this.props.isModal,
+      },
+    ]);
+  };
+
+  renderActions = (info) => {
+    return (
+      <div>
+        <Button
+          onClick={this.editBookModal.bind(this, info.original)}
+          color="primary"
+          style={{ marginRight: 10, marginLeft: 10 }}
+        >
+          <i className="fa fa-edit" />
+        </Button>
+        <Button
+          color="danger"
+          onClick={() => {
+            if (window.confirm("Are you sure?")) {
+            }
+          }}
+        >
+          <i className="fa fa-trash" />
+        </Button>
+      </div>
+    );
+  };
+  columns = [
+    { Header: "Id", accessor: "_id" },
+    { Header: "Name", accessor: "name" },
+    { Header: "Image", accessor: "image" },
+    { Header: "Category", accessor: "category.name" },
+    { Header: "Author", accessor: "author.firstname" },
+    {
+      Header: "Actions",
+      Cell: this.renderActions.bind(this),
+      sortable: false,
+    },
+  ];
+
+  render() {
+    const { books } = this.props;
+
+    return (
+      <ReactTable
+        pageSize={10}
+        columns={this.columns}
+        data={books}
+        getTdProps={() => {
+          return {
+            style: {
+              display: "flex",
+              justifyContent: "center",
+            },
+          };
+        }}
+      />
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  const { books, isBookModal } = state.admin;
+  return { books, isModal: isBookModal };
+};
+
+export default connect(mapStateToProps, {
+  updateAdminProps,
+})(BooksTable);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 import registerReducer from "./registerReducer";
+import adminReducer from "./adminHomeReducer";
 
 export default combineReducers({
   login: loginReducer,
   register: registerReducer,
+  admin: adminReducer,
 });
