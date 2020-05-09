9e7a2fdee3e5b3d84cd51fc8b5bd7f1ceb3c938a
AymanMagdy <magdia883@gmail.com>
2020-05-09 16:05:36 +0200

    doing like sarch for an object

--- routes/search.js
+++ routes/search.js
@@ -10,28 +10,15 @@ app.get('/:q', async (req, res) => {
 
     // Object of the user to sign up.
     search_object = req.params.q
-    const found_by_name = await Book.find({'name': search_object})
-    const found_by_author = await Book.find({'author': search_object})
-    const found_by_category = await Book.find({'category': search_object})
+    // with regex to get the search_object ('like' command).
+    const found_by_name = await Book.find({'name': /^search_object/})
+    
     if (found_by_name) {
         obj_name = {
             message: 'found by name',
             data: found_by_name
         }
         res.send(obj_name) 
-    } else if (found_by_author) {
-        obj_author = {
-            message: 'found by author',
-            data: found_by_author
-        }
-        res.send(obj_author)
-
-    } else if (found_by_category) {
-        obj_category = {
-            message: 'found by category',
-            data: found_by_category
-        }
-        res.send(obj_category)
     }
 })
 
